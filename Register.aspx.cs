35055ae4972000906ce0cb72a89b599d9bcee01d
Ian <Ian@G010812W7>
2017-01-11 16:14:54 +0800

    加入專案檔案。

---
+++ Register.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Segment;
+
+
+
+namespace GoodExample
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Analytics.Initialize("ktxXPGmLDGoLTApP5KInz5oQwWvrXhCU");
+            
+            Analytics.Client.Identify("ianlee0110", new Segment.Model.Traits() {
+            { "name", "nisrin0110" },
+            { "email", "chinglin0110.li@gmail.com" },
+            { "friends", 200 }
+});
+
+        }
+    }
+}
